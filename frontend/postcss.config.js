ee057a13f4dc85b8bfd18e5afe4924d00e6894f1
renn <renatodsantosjr9@gmail.com>
2024-11-25 19:12:46 +0800

    just rest for now LOL

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,9 +1,7 @@
-import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
 
 export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
