9c8d9eeccc08ad04db7c5b71c06db63791a554e8
renn <renatodsantosjr9@gmail.com>
2024-11-25 18:11:26 +0800

    weird bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import * as path from 'path'
 
 const __dirname = path.resolve();
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode }: { mode: string}) => {
   const env = loadEnv(mode, process.cwd())
   
   const backendUrl = env.VITE_BACKEND_URL
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
         '/api': {
           target: backendUrl + '/api',
           changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, '')
+          rewrite: (path: string) => path.replace(/^\/api/, '')
         },
         // for socket
         '/socker.io': {
